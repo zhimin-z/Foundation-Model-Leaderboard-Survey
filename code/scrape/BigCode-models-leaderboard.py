10d0f0665f9101ca439e7421d7028466223cf8b9
JIMMY ZHAO <knightyzhao@gmail.com>
2023-11-17 23:04:50 -0500

    add bigcode

---
+++ code/scrape/BigCode-models-leaderboard.py
@@ -0,0 +1,82 @@
+import argparse
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+import re
+
+from pathlib import Path
+
+path_llm = Path("data/llm")
+
+
+def get_json_format_data():
+    url = 'https://bigcode-bigcode-models-leaderboard.hf.space/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    script_elements = soup.find_all('script')
+    json_format_data = json.loads(str(script_elements[1])[31:-10])
+    return json_format_data
+
+def extract_model_repo_names(html):
+    # This regex pattern looks for the HuggingFace model names in the provided HTML strings.
+    # It matches the text after 'https://huggingface.co/' and before the closing double quote.
+    pattern = r'https://huggingface.co/([^"]+)'
+    matches = re.findall(pattern, html)
+    return matches[0]
+
+
+def get_datas(data):
+    for component_index in range(10, 50, 1): # component_index sometimes changes when they update the space, we can use this "for" loop to avoid changing component index manually
+        try:
+            result_list = []
+            i = 0
+            while True:
+                try:
+                    results = data['components'][component_index]['props']['value']['data'][i]
+                    model = extract_model_repo_names(results[1])
+                    try:
+                        print(len(results))
+                        results_json = {"T": results[0], "Model": model, "Win Rate": results[2], "Average score": results[3], "humaneval-python": results[4], "java": results[5], "javascript": results[6], "Throughput (token/s)": results[7], "cpp": results[8],"php": results[9], "rust": results[10], "swift": results[11], "r": results[12], "lua": results[13], "d": results[14], "racket": results[15], "julia": results[16], "#Languages": results[17], "Throughput (token/s) bs=50": results[18], "Peak Memory (MB)": results[19], "Seq_length": results[20], "Links": results[21], "Submission PR": results[22]}                        
+                    except IndexError: # Wrong component index, so breaking loop to try next component index. (NOTE: More than one component index can give you some results but we must find the right component index to get all results we want.)
+                        break
+                    result_list.append(results_json)
+                    i += 1
+                except IndexError: # No rows to extract so return the list (We know it is the right component index because we didn't break out of loop on the other exception.)
+                    return result_list
+        except (KeyError, TypeError):
+            continue
+
+    return result_list
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Scrape and export data from the Hugging Face leaderboard")
+    parser.add_argument("-csv", action="store_true", help="Export data to CSV")
+    parser.add_argument("-html", action="store_true", help="Export data to HTML")
+    parser.add_argument("-json", action="store_true", help="Export data to JSON")
+    
+    args = parser.parse_args()
+
+    data = get_json_format_data()
+    finished_models = get_datas(data)
+    df = pd.DataFrame(finished_models)
+
+    if not args.csv and not args.html and not args.json:
+        args.json = True  # If no arguments are provided, default to JSON export
+
+    if args.csv:
+        df.to_csv(path_llm / "BigCode-models-leaderboard-2023111.csv", index=False)
+        print("Data exported to CSV")
+
+    if args.html:
+        df.to_html(path_llm / "BigCode-models-leaderboard-2023111.html", index=False)
+        print("Data exported to HTML")
+
+    if args.json:
+        df.to_json(path_llm / "BigCode-models-leaderboard-2023111.json", orient='records', indent=4)
+        print("Data exported to JSON")
+
+if __name__ == "__main__":
+    main()
