833868b4d4dba2107d096fb2593170e7488d9068
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-03 10:59:18 -0500

    add imagenet

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -8,6 +8,8 @@ from pathlib import Path
 
 path_llm = Path("data/llm")
 
+included_links = ['image-classification-on-imagenet', 'self-supervised-image-classification-on', 'image-classification-on-imagenet-real', 'image-classification-on-imagenet-v2', 'domain-generalization-on-imagenet-r', 'zero-shot-transfer-image-classification-on-1', 'zero-shot-transfer-image-classification-on-3', 'zero-shot-transfer-image-classification-on-5', 'zero-shot-transfer-image-classification-on-4']
+
 
 def RefineBaseTitle(title, allUpper=False):
     title = title.replace('-', '_')
@@ -21,19 +23,23 @@ if __name__ == '__main__':
     driver = uc.Chrome()
     driver.implicitly_wait(5)
 
-    dataset = 'beir'
+    leaderboard_links = []
+    dataset = 'imagenet'
     base_url = 'https://paperswithcode.com'
     url = f'{base_url}/dataset/{dataset}'
     driver.get(url)
     
-    leaderboard_tables = driver.find_elements(By.XPATH, '//table[@id="benchmarks-table"]/tbody/tr')
-    leaderboard_links = []
-    
-    for leaderboard in leaderboard_tables:
-        text = leaderboard.get_attribute('onclick')
-        match = re.findall(r"'(.*?)'", text)[0]
-        link = f'{base_url}{match}'
-        leaderboard_links.append(link)
+    if included_links:
+        for match in included_links:
+            link = f'{base_url}/sota/{match}'
+            leaderboard_links.append(link)
+    else:
+        leaderboard_tables = driver.find_elements(By.XPATH, '//table[@id="benchmarks-table"]/tbody/tr')
+        for leaderboard in leaderboard_tables:
+            text = leaderboard.get_attribute('onclick')
+            match = re.findall(r"'(.*?)'", text)[0]
+            link = f'{base_url}{match}'
+            leaderboard_links.append(link)
     
     for link in leaderboard_links:
         driver.get(link)
