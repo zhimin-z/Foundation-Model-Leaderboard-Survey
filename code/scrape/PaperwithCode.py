f8daedae209c79547f64edbde4556ebef6a74c86
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-14 11:00:26 -0500

    add

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -8,15 +8,14 @@ from pathlib import Path
 
 def dataset_rename(name):
     name = name.lower()
-    name = name.split()
-    name = '_'.join(name)
+    name = name.replace(' ', '_')
     return name
 
 def file_rename(folder, dataset, title):
     return '_'.join(title.lower().replace(f' on {dataset.lower()}', '').replace(f' on {folder.lower()}', '').split())
 
 folder = ''
-dataset = 'FewCLUE'
+dataset = 'CoLA'
 path_leaderboard = Path(f"data/{folder}") if folder else Path(f"data/{dataset}")
 
 included_links = []
