d7f395f7a48bacc1bba48674d75ae7b7eef5ad05
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-16 07:04:14 -0500

    add

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -11,11 +11,11 @@ def file_rename(folder, title):
     title = title.replace(' / ', '_').replace(' - ', '_').replace('-', '_').replace(' ', '_')
     return title
 
-folder = 'ADE20K'
-dataset = ''
+folder = ''
+dataset = 'cifar-10'
 path_leaderboard = Path(f"data/{folder}") if folder else Path(f"data/{dataset}")
 
-included_links = ['semantic-segmentation-on-ade20k']
+included_links = []
 
 if __name__ == '__main__':
     driver = uc.Chrome()
---
+++ code/scrape/PaperwithCode_click.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
+import time
+import re
+
+dataset = 'cifar-10'
+
+if __name__ == '__main__':
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-popup-blocking")
+    driver = uc.Chrome(options=chrome_options)
+    base_url = 'https://paperswithcode.com'
+    url = f'{base_url}/dataset/{dataset}'
+    driver.get(url)
+    time.sleep(60)
+    
+    leaderboard_links = []
+    leaderboard_tables = driver.find_elements(By.XPATH, '//table[@id="benchmarks-table"]/tbody/tr')
+    for leaderboard in leaderboard_tables:
+        text = leaderboard.get_attribute('onclick')
+        match = re.findall(r"'(.*?)'", text)[0]
+        link = f'{base_url}{match}'
+        leaderboard_links.append(link)
+
+    for url in leaderboard_links:
+        driver.execute_script(f"window.open('{url}');")
+    
+    time.sleep(100000)
+                
