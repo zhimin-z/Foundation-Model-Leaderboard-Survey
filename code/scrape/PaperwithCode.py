42a10cf00b12f38642d6ae615f5dcc9f0f44daba
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-16 22:00:31 -0500

    add

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -11,9 +11,9 @@ def file_rename(folder, title):
     title = title.replace(' / ', '_').replace(' - ', '_').replace('-', '_').replace(' ', '_')
     return title
 
-folder = 'STS-B'
-dataset = 'sts-benchmark'
-included_links = ['semantic-textual-similarity-on-sts-benchmark']
+folder = 'Natural Questions'
+dataset = 'natural-questions'
+included_links = []
 
 if __name__ == '__main__':
     driver = uc.Chrome()
