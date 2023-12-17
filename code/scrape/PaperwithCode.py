23593e9f27d9961c1fbc44da5cab287170ebb01f
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-17 15:15:52 -0500

    add

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -11,8 +11,8 @@ def file_rename(folder, title):
     title = title.replace(' / ', '_').replace(' - ', '_').replace('-', '_').replace(' ', '_')
     return title
 
-folder = 'RTE'
-dataset = 'rte'
+folder = 'StrategyQA'
+dataset = 'strategyqa'
 included_links = []
 
 if __name__ == '__main__':
