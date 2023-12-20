4794b58180174eade6b8e641b571b603f559ed05
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-20 14:50:52 -0500

    add

--- code/scrape/LMExamQA.py
+++ code/scrape/LMExamQA.py
@@ -1,20 +1,12 @@
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
-
-from pathlib import Path
-
-from selenium.webdriver.chrome.options import Options
 import os
 import time
 
-options = Options()
-download_path = Path("data/LMExamQA")
-options.add_experimental_option("prefs", {
-    "download.default_directory": str(download_path),
-    "download.prompt_for_download": False,
-    # "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-})
+def preprocess_name(s):
+    return ' '.join(s.lower().split[:-1])
+
+path_leaderboard = "data/LMExamQA"
 
 def get_latest_file(dir_path):
     files = os.listdir(dir_path)
@@ -22,49 +14,50 @@ def get_latest_file(dir_path):
     return max(paths, key=os.path.getctime)
 
 
-def download(driver, menu):
+def download(driver, table):
+    table.click()
+    menu = driver.find_element(By.XPATH, '//button[@class="highcharts-a11y-proxy-element highcharts-no-tooltip"]')
     menu.click()
-    time.sleep(0.2)
-    # wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
-    # try:
-    #     menu = driver.find_element(By.XPATH, '//button[@class="highcharts-a11y-proxy-element highcharts-no-tooltip"]')
-    # except:
-    #     menu = driver.find_element(By.XPATH, '//div[@class="highcharts-contextmenu"]')
-    # menu.click()
-    # # menu = driver.find_element(By.XPATH, '//g[@class="highcharts-exporting-group"]')
-    # # except:
-    # #     menu = driver.find_element(By.XPATH, '//g[@class="highcharts-no-tooltip highcharts-button highcharts-contextbutton highcharts-button-normal"]')
-    # #     menu.click()
-    # table = driver.find_elements(By.XPATH, '//li[@class="highcharts-menu-item"]')[6]
-    # print(table.text)
-    # table.click()
+    download = driver.find_elements(By.XPATH, '//li[@class="highcharts-menu-item"]')[6]
+    download.click()
+    time.sleep(5)
+    latest_file = get_latest_file(path_leaderboard)
+    os.rename(latest_file, f'{path_leaderboard}/shw-{preprocess_name(table.text)}.csv')
+
 
 if __name__ == '__main__':
-    driver = uc.Chrome()#options=options)
+    driver = uc.Chrome()
+
+    params = {
+        "behavior": "allow",
+        "downloadPath": path_leaderboard
+    }
+    driver.execute_cdp_cmd("Page.setDownloadBehavior", params)
+
     driver.implicitly_wait(5)
 
     base_url = 'https://lmexam.com/'
+
+    # driver.execute_cdp_cmd(
+    #     "Browser.grantPermissions",
+    #     {
+    #         "origin": base_url,
+    #         "permissions": ["geolocation"]
+    #     },
+    # )
+
     driver.get(base_url)
-    
-    result = driver.find_element(By.XPATH, '//button[@id="pills-result-tab"]')
-    download(driver, result)
-    # time.sleep(10)  # Adjust sleep time as needed
-    # original_file_name = os.path.join(download_path, "original_file_name.ext")
-    # new_file_name = os.path.join(download_path, "new_file_name.ext")
-    # os.rename(original_file_name, new_file_name)
-    
-    for domain in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-primary"]'):
-        print(domain.text)
-        download(driver, domain)
-        # time.sleep(1)
-        for subdomain in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-danger"]'):
-            print(subdomain.text)
-            download(driver, subdomain)
-            # time.sleep(1)
-            for subsubdomain in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-warning"]'):
-                print(subsubdomain.text)
-                download(driver, subsubdomain)
-                # time.sleep(1)
-                for subsubsubdomain in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-success"]'):
-                    print(subsubsubdomain.text)
-                    download(driver, subsubsubdomain)
+
+    zero = driver.find_element(By.XPATH, '//button[@id="pills-result-tab"]')
+    download(driver, zero)
+
+    for first in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-primary"]'):
+        download(driver, first)
+        for second in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-danger"]'):
+            download(driver, second)
+            for third in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-warning"]'):
+                download(driver, third)
+                for fourth in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-success"]'):
+                    download(driver, fourth)
+                    for fifth in driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-info"]'):
+                        download(driver, fifth)
