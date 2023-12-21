3ba33cce193598012149e2e1f757aa5c412002b2
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-21 02:11:40 -0500

    add

--- code/scrape/HELM.py
+++ code/scrape/HELM.py
@@ -4,14 +4,6 @@ import undetected_chromedriver as uc
 import pandas as pd
 
 path_leaderboard = "data/HELM"
-# substrings_to_remove = [" \u2191", " \u2193"]
-
-
-# def remove_substrings(s, substrings=substrings_to_remove):
-#     for substring in substrings:
-#         s = s.replace(substring, '')
-#     return s
-
 
 def prepcess_name(s):
     s = s.lower()
@@ -35,50 +27,24 @@ if __name__ == '__main__':
             select.select_by_index(index)
             group_name = prepcess_name(select.first_selected_option.text)
             
-            for table in driver.find_elements(By.XPATH, '//div[@role="navigation"]/div'):
-                table.click()
-                table_name = prepcess_name(table.text)
+            for scenario in driver.find_elements(By.XPATH, '//div[@role="navigation"]/div'):
+                scenario.click()
+                table_name = prepcess_name(scenario.text)
                 
+                table = driver.find_element(By.XPATH, f'//table[@class="table w-full px-4"]')
                 column_names = [column.text for column in table.find_elements(By.XPATH, './/span')]
                 
                 df = []
-                content = driver.find_element(By.XPATH, '//table[@class="table w-full px-4"]')
-                for row in content.find_elements(By.XPATH, './/tbody/tr'):
+                for row in table.find_elements(By.XPATH, './/tbody/tr'):
                     series = {}
                     values = row.find_elements(By.XPATH, './/td')
                     for column_name, value in zip(column_names, values):
                         series[column_name] = value.text
                     df.append(series)
-                    
+                 
                 df = pd.DataFrame(df)
-                df.to_json(f'{path_leaderboard}/shw-{group_name}-{table_name}.json', orient='records', indent=4)
-            break
-        break
-
-    # for scenario in scenarios.find_elements(By.XPATH, './/a'):
-    #     scenario_url_lst.add(scenario.get_attribute('href'))
-        
-    # for scenario_url in scenario_url_lst:
-    #     driver.get(scenario_url)
-    #     print(scenario_url)
-        
-    #     table_names = driver.find_elements(By.XPATH, '//div[@class="table-container"]')
-    #     if len(table_names) < 2:
-    #         table_names = [driver.find_element(By.XPATH, '//div[@class="col-sm-12"]/div/h3').text]
-    #     else:
-    #         table_names = [table_name.get_attribute('id') for table_name in table_names]
-            
-    #     tables = driver.find_elements(By.XPATH, '//table[@class="query-table results-table"]')
-    #     for table_name, table in zip(table_names, tables):
-    #         column_names = [remove_substrings(column.text) for column in table.find_elements(By.XPATH, './/thead/tr/td/span')]
-            
-    #         df = []
-    #         for row in table.find_elements(By.XPATH, './/tbody/tr'):
-    #             series = {}
-    #             values = row.find_elements(By.XPATH, './/span')
-    #             for column_name, value in zip(column_names, values):
-    #                 series[column_name] = value.text
-    #             df.append(series)
-
-    #         df = pd.DataFrame(df)
-    #         df.to_json(f'{path_leaderboard}/shw-{prepcess_name(table_name)}.json', orient='records', indent=4)
+                if table_name:
+                    file_name = f'{path_leaderboard}/shw-{version}-{group_name}-{table_name}.json'
+                else:
+                    file_name = f'{path_leaderboard}/shw-{version}-{group_name}.json'
+                df.to_json(file_name, orient='records', indent=4)
