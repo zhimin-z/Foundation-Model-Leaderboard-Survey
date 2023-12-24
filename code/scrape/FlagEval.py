88780ba71604cf26031e8a9678cf4f728a943857
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-24 10:11:07 -0500

    add

--- code/scrape/FlagEval.py
+++ code/scrape/FlagEval.py
@@ -14,6 +14,9 @@ def prepcess_name(s):
     return s
 
 
+def concatenate_elements(*args):
+    return '\n'.join(filter(None, args))
+
 if __name__ == '__main__':
     driver = uc.Chrome()
     driver.implicitly_wait(10)
@@ -26,44 +29,71 @@ if __name__ == '__main__':
     
     english = driver.find_elements(By.XPATH, '//li[@class="el-dropdown-menu__item"]')[1]
     english.click()
-            
-    # domain_list = Select()
+    
     for domain in driver.find_element(By.XPATH, '//div[@class="el-form-item__content"]').find_elements(By.XPATH, './/div'):
         domain_name = domain.text
         print(domain.text)
-        # domain_list.select_by_visible_text(domain_name)
         domain.click()
-        # time.sleep(1)
-        # driver.execute_script("arguments[0].click();", domain)
         metric_list = driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[1].find_elements(By.XPATH, './/div')
         if metric_list:
-            print('Metrics:')
             for metric in metric_list:
                 metric_name = metric.text
                 print(metric.text)
-                # metric_list.select_by_visible_text(metric_name)
                 metric.click()
-                # time.sleep(1)
-                # driver.execute_script("arguments[0].click();", metric)
-                type_list = driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[2].find_elements(By.XPATH, './/div')
-                if type_list:
-                    print('Model Types:')
-                    for type in type_list:
+                try:
+                    # type_list = driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[2].find_elements(By.XPATH, './/div')
+                    for type in driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[2].find_elements(By.XPATH, './/div'):
                         type_name = type.text
                         print(type.text)
-                        # type_list.select_by_visible_text(type_name)
                         type.click()
-                        # time.sleep(1)
-                        # driver.execute_script("arguments[0].click();", type)
-                        column_names = []
-                        upper_columns, lower_columns = driver.find_elements(By.XPATH, '//thead[@class="is-group"]/tr')
-                        for column in upper_columns.find_elements(By.XPATH, './/th'):
-                            if column.get_attribute('colspan') == '1':
-                                column_names.append(column.text)
-                        for column in lower_columns.find_elements(By.XPATH, './/th'):
-                            column_names.append(column.text)
-                        print(column_names)
-                else:
+                        time.sleep(0.5)
+                        
+                        upper_columns_mapping = []
+                        middle_columns_mapping = []
+                        upper_columns_list = []
+                        middle_columns_list = []
+                        lower_columns_list = []
+                        column_list = driver.find_elements(By.XPATH, '//thead[@class="is-group"]/tr')
+                        match len(column_list):
+                            case 3:
+                                upper_columns, middle_columns, lower_columns = column_list
+                                for column in upper_columns.find_elements(By.XPATH, './/th'):
+                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
+                                    upper_columns_mapping.append(value)
+                                for column in middle_columns.find_elements(By.XPATH, './/th'):
+                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
+                                    middle_columns_mapping.append(value)
+                                for column in lower_columns.find_elements(By.XPATH, './/th'):
+                                    lower_columns_list.append(driver.execute_script("return arguments[0].innerText;", column))
+                                for name, frequency in upper_columns_mapping:
+                                    upper_columns_list.extend([name] * int(frequency))
+                                for name, frequency in middle_columns_mapping:
+                                    middle_columns_list.extend([name] * int(frequency))
+                                middle_columns_list = [''] * (len(upper_columns_list) - len(middle_columns_list)) + middle_columns_list
+                                lower_columns_list = [''] * (len(upper_columns_list) - len(lower_columns_list)) + lower_columns_list
+                                column_names = [concatenate_elements(a, b, c) for a, b, c in zip(upper_columns_list, middle_columns_list, lower_columns_list)]
+                            case 2:
+                                upper_columns, lower_columns = column_list
+                                for column in upper_columns.find_elements(By.XPATH, './/th'):
+                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
+                                    upper_columns_mapping.append(value)
+                                for column in lower_columns.find_elements(By.XPATH, './/th'):
+                                    lower_columns_list.append(driver.execute_script("return arguments[0].innerText;", column))
+                                for name, frequency in upper_columns_mapping:
+                                    upper_columns_list.extend([name] * int(frequency))
+                                lower_columns_list = [''] * (len(upper_columns_list) - len(lower_columns_list)) + lower_columns_list
+                                column_names = [concatenate_elements(a, b) for a, b in zip(upper_columns_list, lower_columns_list)]
+                            case _:
+                                print('No data!')
+                        df = []
+                        for row in driver.find_elements(By.XPATH, "//*[contains(@class, 'el-table__row')]"):
+                            values = []
+                            for value in row.find_elements(By.XPATH, './/td'):
+                                values.append(driver.execute_script("return arguments[0].innerText;", value))
+                            df.append(values)
+                        df = pd.DataFrame(df, columns=column_names)
+                        df.to_json(f'{path_leaderboard}/shw-{prepcess_name(domain_name)}-{prepcess_name(metric_name)}-{prepcess_name(type_name)}.json', orient='records', indent=4)
+                except:
                     print('No model types')
         else:
             print('No metrics')
