5762dbc567f828baa4aa8d8d01a2c88255778cb5
JIMMY ZHAO <knightyzhao@gmail.com>
2023-12-24 19:43:20 -0500

    add

--- code/scrape/FlagEval.py
+++ code/scrape/FlagEval.py
@@ -13,9 +13,73 @@ def prepcess_name(s):
     s = s.replace(" ", "_")
     return s
 
+def concatenate_elements(*expanded_lists):
+    # Ensure all lists are of the same length
+    length_of_lists = len(expanded_lists[0])
+    if not all(len(lst) == length_of_lists for lst in expanded_lists):
+        raise ValueError("All lists must be of the same length.")
 
-def concatenate_elements(*args):
-    return '\n'.join(filter(None, args))
+    # Extract the first element of each tuple in each list
+    extracted_elements = [[elem[0] for elem in lst] for lst in expanded_lists]
+
+    # Concatenate elements with the same index, ignoring empty strings
+    concatenated_results = []
+    for i in range(length_of_lists):
+        elements = [extracted_elements[j][i] for j in range(len(expanded_lists)) if extracted_elements[j][i]]
+        concatenated_results.append("\n".join(elements))
+
+    return concatenated_results
+
+def retrieve_table(driver):
+    column_list = driver.find_elements(By.XPATH, '//thead[@class="is-group"]/tr')
+    match len(column_list):
+        case 3:
+            upper_columns_mapping = []
+            middle_columns_mapping = []
+            lower_columns_mapping = []
+            upper_columns, middle_columns, lower_columns = column_list
+            for column in upper_columns.find_elements(By.XPATH, './/th'):
+                value = (driver.execute_script("return arguments[0].innerText;", column), int(column.get_attribute('colspan')), int(column.get_attribute('rowspan')))
+                upper_columns_mapping.append(value)
+            for column in middle_columns.find_elements(By.XPATH, './/th'):
+                value = (driver.execute_script("return arguments[0].innerText;", column), int(column.get_attribute('colspan')), int(column.get_attribute('rowspan')))
+                middle_columns_mapping.append(value)
+            for column in lower_columns.find_elements(By.XPATH, './/th'):
+                value = (driver.execute_script("return arguments[0].innerText;", column), int(column.get_attribute('colspan')), int(column.get_attribute('rowspan')))
+                lower_columns_mapping.append(value)
+            for index, (_, rowspan) in enumerate(upper_columns_mapping):
+                if rowspan == 2:
+                    middle_columns_mapping.insert(index, ('', 1))
+                elif rowspan == 3:
+                    middle_columns_mapping.insert(index, ('', 1))
+                    lower_columns_mapping.insert(index, ('', 1))
+            for index, (_, rowspan) in enumerate(middle_columns_mapping):
+                if rowspan == 2:
+                    lower_columns_mapping.insert(index, ('', 1))
+            column_names = concatenate_elements(upper_columns_mapping, middle_columns_mapping, lower_columns_mapping)
+        case 2:
+            upper_columns_mapping = []
+            middle_columns_mapping = []
+            upper_columns, middle_columns = column_list
+            for column in upper_columns.find_elements(By.XPATH, './/th'):
+                value = (driver.execute_script("return arguments[0].innerText;", column), int(column.get_attribute('colspan')), int(column.get_attribute('rowspan')))
+                upper_columns_mapping.append(value)
+            for column in middle_columns.find_elements(By.XPATH, './/th'):
+                value = (driver.execute_script("return arguments[0].innerText;", column), int(column.get_attribute('colspan')), int(column.get_attribute('rowspan')))
+                middle_columns_mapping.append(value)
+            for index, (_, rowspan) in enumerate(upper_columns_mapping):
+                if rowspan == 2:
+                    middle_columns_mapping.insert(index, ('', 1))
+            column_names = concatenate_elements(upper_columns_mapping, middle_columns_mapping)
+        case _:
+            return pd.DataFrame()
+    df = []
+    for row in driver.find_elements(By.XPATH, "//*[contains(@class, 'el-table__row')]"):
+        values = []
+        for value in row.find_elements(By.XPATH, './/td'):
+            values.append(driver.execute_script("return arguments[0].innerText;", value))
+        df.append(values)
+    return pd.DataFrame(df, columns=column_names)
 
 if __name__ == '__main__':
     driver = uc.Chrome()
@@ -33,6 +97,8 @@ if __name__ == '__main__':
     for domain in driver.find_element(By.XPATH, '//div[@class="el-form-item__content"]').find_elements(By.XPATH, './/div'):
         domain_name = domain.text
         print(domain.text)
+        if domain_name == 'NLP':
+            continue
         domain.click()
         metric_list = driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[1].find_elements(By.XPATH, './/div')
         if metric_list:
@@ -41,60 +107,22 @@ if __name__ == '__main__':
                 print(metric.text)
                 metric.click()
                 try:
-                    # type_list = driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[2].find_elements(By.XPATH, './/div')
                     for type in driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')[2].find_elements(By.XPATH, './/div'):
                         type_name = type.text
                         print(type.text)
                         type.click()
-                        time.sleep(0.5)
-                        
-                        upper_columns_mapping = []
-                        middle_columns_mapping = []
-                        upper_columns_list = []
-                        middle_columns_list = []
-                        lower_columns_list = []
-                        column_list = driver.find_elements(By.XPATH, '//thead[@class="is-group"]/tr')
-                        match len(column_list):
-                            case 3:
-                                upper_columns, middle_columns, lower_columns = column_list
-                                for column in upper_columns.find_elements(By.XPATH, './/th'):
-                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
-                                    upper_columns_mapping.append(value)
-                                for column in middle_columns.find_elements(By.XPATH, './/th'):
-                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
-                                    middle_columns_mapping.append(value)
-                                for column in lower_columns.find_elements(By.XPATH, './/th'):
-                                    lower_columns_list.append(driver.execute_script("return arguments[0].innerText;", column))
-                                for name, frequency in upper_columns_mapping:
-                                    upper_columns_list.extend([name] * int(frequency))
-                                for name, frequency in middle_columns_mapping:
-                                    middle_columns_list.extend([name] * int(frequency))
-                                middle_columns_list = [''] * (len(upper_columns_list) - len(middle_columns_list)) + middle_columns_list
-                                lower_columns_list = [''] * (len(upper_columns_list) - len(lower_columns_list)) + lower_columns_list
-                                column_names = [concatenate_elements(a, b, c) for a, b, c in zip(upper_columns_list, middle_columns_list, lower_columns_list)]
-                            case 2:
-                                upper_columns, lower_columns = column_list
-                                for column in upper_columns.find_elements(By.XPATH, './/th'):
-                                    value = (driver.execute_script("return arguments[0].innerText;", column), column.get_attribute('colspan'))
-                                    upper_columns_mapping.append(value)
-                                for column in lower_columns.find_elements(By.XPATH, './/th'):
-                                    lower_columns_list.append(driver.execute_script("return arguments[0].innerText;", column))
-                                for name, frequency in upper_columns_mapping:
-                                    upper_columns_list.extend([name] * int(frequency))
-                                lower_columns_list = [''] * (len(upper_columns_list) - len(lower_columns_list)) + lower_columns_list
-                                column_names = [concatenate_elements(a, b) for a, b in zip(upper_columns_list, lower_columns_list)]
-                            case _:
-                                print('No data!')
-                        df = []
-                        for row in driver.find_elements(By.XPATH, "//*[contains(@class, 'el-table__row')]"):
-                            values = []
-                            for value in row.find_elements(By.XPATH, './/td'):
-                                values.append(driver.execute_script("return arguments[0].innerText;", value))
-                            df.append(values)
-                        df = pd.DataFrame(df, columns=column_names)
-                        df.to_json(f'{path_leaderboard}/shw-{prepcess_name(domain_name)}-{prepcess_name(metric_name)}-{prepcess_name(type_name)}.json', orient='records', indent=4)
+                        # time.sleep(0.5)
+                        df = retrieve_table(driver)
+                        if not df.empty:
+                            df.to_json(f'{path_leaderboard}/shw-{prepcess_name(domain_name)}-{prepcess_name(metric_name)}-{prepcess_name(type_name)}.json', orient='records', indent=4)
                 except:
+                    df = retrieve_table(driver)
+                    if not df.empty:
+                        df.to_json(f'{path_leaderboard}/shw-{prepcess_name(domain_name)}-{prepcess_name(metric_name)}.json', orient='records', indent=4)
                     print('No model types')
         else:
+            df = retrieve_table(driver)
+            if not df.empty:
+                df.to_json(f'{path_leaderboard}/shw-{prepcess_name(domain_name)}.json', orient='records', indent=4)
             print('No metrics')
     
