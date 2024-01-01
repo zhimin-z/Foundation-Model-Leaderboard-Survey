5f4d7500c8c71de5b65d6bd8a10bffabcdfd36ab
JIMMY ZHAO <knightyzhao@gmail.com>
2024-01-01 13:46:14 -0500

    storycloze

--- code/scrape/PaperwithCode.py
+++ code/scrape/PaperwithCode.py
@@ -11,8 +11,8 @@ def file_rename(folder, title):
     title = title.replace(', ', '_').replace(' / ', '_').replace('/', '_').replace(' - ', '_').replace('-', '_').replace(' ', '_')
     return title
 
-folder = 'SICK'
-dataset = 'sick'
+folder = 'StoryCloze'
+dataset = 'storycloze'
 included_links = []
 
 if __name__ == '__main__':
