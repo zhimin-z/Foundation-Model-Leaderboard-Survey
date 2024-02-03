87edfd047e43c10943cbdef1fd43a18add7e070e
JIMMY ZHAO <knightyzhao@gmail.com>
2024-02-03 02:33:39 -0500

    add CCEval

--- code/scrape/Coding LLMs Leaderboard.py
+++ code/scrape/Coding LLMs Leaderboard.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 import pandas as pd
 
+path_leaderboard = 'data/CCEval'
+leaderboard_names = ['overall', 'instruct_fine_tuning', 'model_type']
+
 if __name__ == '__main__':
     driver = uc.Chrome()
     actions = ActionChains(driver)
@@ -12,19 +15,22 @@ if __name__ == '__main__':
     url = 'https://leaderboard.tabbyml.com'
     driver.get(url)
     
-    column_names = ['Model']
-    for column_name in driver.find_elements(By.XPATH, '//div[@class="flex flex-col font-thin"]/div'):
-        column_names.append(column_name.text)
+    for leaderboard in driver.find_elements(By.XPATH, '//div[@class="xl:fixed xl:bottom-0 my-10 flex flex-col gap-4 xl:flex-row"]/div'):
+        leaderboard.click()
     
-    models = driver.find_elements(By.XPATH, '//p[@class="font-sf tracking-wide md:w-48 md:mr-6 md:text-right"]')
-    perfs = driver.find_elements(By.XPATH, './/div[@class="text-xs py-1 xl:py-0"]')
-    df = []
-    for model, perf in zip(models, perfs):
-        actions.move_to_element(perf).perform()
-        row = [model.text]
-        for value in perf.find_elements(By.XPATH, ".//*[contains(@class, 'flex items-center gap-2')]"):
-            row.append(value.text)
-        df.append(row)
+        column_names = ['Model']
+        for column_name in driver.find_elements(By.XPATH, '//div[@class="flex flex-col font-thin"]/div'):
+            column_names.append(column_name.text)
     
-    df = pd.DataFrame(df, columns=column_names)
-    df.to_json('data/Coding LLMs Leaderboard/shw.json', orient='records', indent=4)
+        models = driver.find_elements(By.XPATH, '//p[@class="font-sf tracking-wide md:w-48 md:mr-6 md:text-right"]')
+        perfs = driver.find_elements(By.XPATH, './/div[@class="text-xs py-1 xl:py-0"]')
+        df = []
+        for model, perf in zip(models, perfs):
+            actions.move_to_element(perf).perform()
+            row = [model.text]
+            for value in perf.find_elements(By.XPATH, ".//*[contains(@class, 'flex items-center gap-2')]"):
+                row.append(value.text)
+            df.append(row)
+    
+        df = pd.DataFrame(df, columns=column_names)
+        df.to_json(f'{path_leaderboard}/shw-{leaderboard_names.pop(0)}.json', orient='records', indent=4)
