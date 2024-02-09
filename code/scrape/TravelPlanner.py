3dbaa3d74eebe98092db6c32f65d5365f856fad6
JIMMY ZHAO <knightyzhao@gmail.com>
2024-02-08 23:44:58 -0500

    add trustllm+donotanswer

--- code/scrape/TravelPlanner.py
+++ code/scrape/TravelPlanner.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import requests
 import json
-import re
 
 from pathlib import Path
 from bs4 import BeautifulSoup
---
+++ code/scrape/TrustLLM.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+import pandas as pd
+
+path_leaderboard = "data/TrustLLM"
+
+
+# def prepcess_name(s):
+#     s = s.lower()
+#     s = s.replace('leaderboard - ', '')
+#     s = s.replace(' ', '_')
+#     return s
+
+if __name__ == '__main__':
+    driver = uc.Chrome()
+    driver.implicitly_wait(5)
+
+    url = 'https://trustllmbenchmark.github.io/TrustLLM-Website/leaderboard.html'
+    driver.get(url)
+    
+    leaderboard_names = [name.text.lower() for name in driver.find_elements(By.XPATH, '//div[@class="section"]/h2')]
+    
+    for leaderboard in driver.find_elements(By.XPATH, ".//table[contains(@id, 'table_')]"):
+        column_names = []
+        for column in leaderboard.find_elements(By.XPATH, './/thead/tr/th'):
+            column_name = column.text.replace(' (↑)', '')
+            column_names.append(column_name)
+        
+        df = []
+        for row in leaderboard.find_elements(By.XPATH, './/tbody/tr'):
+            values = [column.text for column in row.find_elements(By.XPATH, './/td')]
+            df.append(values)
+        
+        df = pd.DataFrame(df, columns=column_names)
+        df.to_json(f'{path_leaderboard}/gh-{leaderboard_names.pop(0)}.json', orient='records', indent=4)
+        
