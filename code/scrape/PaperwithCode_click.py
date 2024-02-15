1e5ebb4139dfdbd0a8cde4ce76eaa067f2e51fd1
JIMMY ZHAO <knightyzhao@gmail.com>
2024-02-15 15:13:03 -0500

    add common voice

--- code/scrape/PaperwithCode_click.py
+++ code/scrape/PaperwithCode_click.py
@@ -4,8 +4,8 @@ import undetected_chromedriver as uc
 import time
 import re
 
-max_page = -1
 dataset = 'common-voice'
+stop_link = 'automatic-speech-recognition-on-mozilla-65'
 
 if __name__ == '__main__':
     chrome_options = Options()
@@ -14,18 +14,18 @@ if __name__ == '__main__':
     base_url = 'https://paperswithcode.com'
     url = f'{base_url}/dataset/{dataset}'
     driver.get(url)
-    time.sleep(30)
     
     leaderboard_links = []
     leaderboard_tables = driver.find_elements(By.XPATH, '//table[@id="benchmarks-table"]/tbody/tr')
     for leaderboard in leaderboard_tables:
         text = leaderboard.get_attribute('onclick')
         match = re.findall(r"'(.*?)'", text)[0]
+        if stop_link and (stop_link in match):
+            break
         link = f'{base_url}{match}'
         leaderboard_links.append(link)
 
-    max_page = len(leaderboard_links) if max_page == -1 else max_page
-    for url in leaderboard_links[:max_page]:
+    for url in leaderboard_links:
         driver.execute_script(f"window.open('{url}');")
     
     time.sleep(100000)
