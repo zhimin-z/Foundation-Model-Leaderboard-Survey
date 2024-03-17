cfa40ad083de9b4111723d7626e9bb98d8ba6f16
zhimin-z <knightyzhao@gmail.com>
2024-03-17 15:06:14 -0400

    add Nexus Function Calling

--- code/scrape/Nexus Function Calling.py
+++ code/scrape/Nexus Function Calling.py
@@ -6,25 +6,15 @@ import os
 
 from bs4 import BeautifulSoup
 
-path_leaderboard = "data/LLM-Perf Leaderboard"
-leaderboard_names = ['a100_80gb_275w', 'rtx4090_24gb_450w']
-
-
-def filter_string(text):
-    regex_pattern = r'🤗|🏛️|📥|🏭|🛠️|🗜️'
-    filtered_string = re.sub(regex_pattern, '', text)
-    return filtered_string.strip()
+url = 'https://nexusflow-nexus-function-calling-leaderboard.hf.space'
+path_leaderboard = "data/Nexus Function Calling"
+leaderboard_names = ['overall', 'climate-nested_parallel', 'stack-mostly_single', 'places-nested', 'otx-single', 'virustotal-single', 'vt_multi-nested', 'vt_multi-parallel', 'nvdlibrary-single', 'nvdlibrary_multi-nested']
 
 
 def get_json_format_data(script_elements):
     for item in json.loads(str(script_elements[1])[31:-10])['components']:
         if item['type'] == 'dataframe':
-            columns = [filter_string(column)
-                       for column in item['props']['headers']]
-            data = item['props']['value']['data']
-            df = pd.DataFrame(data, columns=columns)
-            df['Model'] = df['Model'].apply(
-                lambda x: extract_model_repo_names(x))
+            df = pd.DataFrame(item['props']['value']['data'], columns=item['props']['value']['headers'])
             df.to_json(f'{path_leaderboard}/hf-{leaderboard_names.pop(0)}.json', orient='records', indent=4)
 
 
@@ -39,7 +29,7 @@ def extract_model_repo_names(html):
 if __name__ == "__main__":
     if not os.path.exists(path_leaderboard):
         os.makedirs(path_leaderboard)
-    url = 'https://optimum-llm-perf-leaderboard.hf.space'
+        
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     script_elements = soup.find_all('script')
