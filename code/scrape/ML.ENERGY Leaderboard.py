9a2551d1f551863c21ef51b26f2c688f80eb1775
zhimin-z <knightyzhao@gmail.com>
2024-03-19 19:22:15 -0400

    add

---
+++ code/scrape/ML.ENERGY Leaderboard.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+
+from selenium.webdriver.common.by import By
+from seleniumbase import Driver
+
+path_leaderboard = "data/ML.ENERGY Leaderboard"
+
+
+if __name__ == '__main__':
+    if not os.path.exists(path_leaderboard):
+        os.makedirs(path_leaderboard)
+    
+    driver = Driver(uc=True)
+    driver.implicitly_wait(5)
+
+    url = 'https://ml.energy/leaderboard'
+    driver.get(url)
+        
+    table = driver.find_element(By.XPATH, '//table[@class="table table-responsive"]')
+    column_names = ['Date']
+    for column in table.find_elements(By.XPATH, './/thead/tr/th'):
+        if column.text.strip():
+            column_names.append(column.text)
+
+    df = []
+    for row in table.find_elements(By.XPATH, './/tbody/tr'):
+        values = []
+        for name, value in zip(column_names, row.find_elements(By.XPATH, './/td')):
+            if name == 'Date':
+                values.append(value.text.split('\n')[-1])
+            elif name == 'Code':
+                try:
+                    values.append(value.find_element(By.XPATH, './/a').get_attribute('href'))
+                except:
+                    values.append('')
+            else:
+                values.append(value.text)
+        df.append(values)
+        
+    df = pd.DataFrame(df, columns=column_names)
+    df.to_json(f'{path_leaderboard}/gh.json', orient='records', indent=4)
