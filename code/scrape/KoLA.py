b0213000c220114fe175139d4bd5c6611c2d4b41
zhimin-z <knightyzhao@gmail.com>
2024-03-21 21:27:37 -0400

    update

--- code/scrape/KoLA.py
+++ code/scrape/KoLA.py
@@ -2,13 +2,17 @@ import pandas as pd
 import json
 import os
 
-path_leaderboard = 'data/KoLA/ip.json'
+path_leaderboard = 'data/KoLA'
+original_filename = 'result.json'
 
 if __name__ == '__main__':
     if not os.path.exists(path_leaderboard):
         os.makedirs(path_leaderboard)
         
-    with open(path_leaderboard, 'r') as f:
+    with open(f'{path_leaderboard}/{original_filename}', 'r') as f:
         data = json.load(f)
         df = pd.DataFrame(data['result'])
-        df.to_json(path_leaderboard, orient='records', indent=4)
+        df.drop(columns=['rank'], inplace=True)
+        df.rename(columns={'model': 'Model'}, inplace=True)
+        df.to_json(f'{path_leaderboard}/ip.json', orient='records', indent=4)
+    os.remove(f'{path_leaderboard}/{original_filename}')
