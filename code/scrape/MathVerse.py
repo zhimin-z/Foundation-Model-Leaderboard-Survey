0d8fcd71b90fdf34b81943cf31c6eca27bf42765
JIMMY ZHAO <knightyzhao@gmail.com>
2024-04-19 12:03:05 -0400

    add mathverse

--- code/scrape/MathVerse.py
+++ code/scrape/MathVerse.py
@@ -4,7 +4,7 @@ import os
 from selenium.webdriver.common.by import By
 from seleniumbase import Driver
 
-path_leaderboard = "data/TabMWP"
+path_leaderboard = "data/MathVerse"
 
 if __name__ == '__main__':
     if not os.path.exists(path_leaderboard):
@@ -13,11 +13,22 @@ if __name__ == '__main__':
     driver = Driver(uc=True)
     driver.implicitly_wait(5)
 
-    url = 'https://promptpg.github.io/leaderboard.html'
+    url = 'https://mathverse-cuhk.github.io'
     driver.get(url)
 
     table = driver.find_element(By.XPATH, '//table[@class="js-sort-table"]')
-    column_names = [column.text for column in table.find_elements(By.XPATH, './/thead/tr/td')]
+        
+
+    column_names = []
+    upper_columns, lower_columns = table.find_elements(By.XPATH, '//thead/tr')
+    lower_column_list = [lower_column.text for lower_column in lower_columns.find_elements(By.XPATH, './/th')]
+    for upper_column in upper_columns.find_elements(By.XPATH, './/th'):
+        try:
+            for _ in range(int(upper_column.get_attribute('colspan'))):
+                column_names.append(f'{upper_column.text} ({lower_column_list.pop(0)})')
+        except:
+            column_names.append(upper_column.text)
+        
 
     df = []
     for row in table.find_elements(By.XPATH, './/tbody/tr'):
@@ -32,3 +43,5 @@ if __name__ == '__main__':
     df = pd.DataFrame(df, columns=column_names)
     df.drop(columns=['#'], inplace=True)
     df.to_json(f'{path_leaderboard}/gh.json', orient='records', indent=4)
+    
+    driver.quit()
