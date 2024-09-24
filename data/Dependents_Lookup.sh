cbf21034f92a35fe76169e7a029741067764ec27
JIMMY ZHAO <knightyzhao@gmail.com>
2024-09-24 15:55:22 -0400

    add intermediate resutls

---
+++ data/Dependents_Lookup.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# curl -L https://sourcegraph.com/.api/src-cli/src_darwin_amd64 -o src
+# chmod +x src
+
+# Get the path_repository argument
+path_repository="$1"
+
+src search -json 'context:global "leaderboard" select:repo count:all file:\.(html|md)' >"${path_repository}/GitHub.json"
